b2e0caf567995f200d373854a1b516c18bc791b8
yingjhu <yingjhu@hotmail.com>
2014-10-14 15:52:23 +0800

    Erased

---
+++ Erased/src/Erased.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Erased {
+
+	public static void main(String[] args) {
+		Class c1 = new ArrayList<String>().getClass();
+		Class c2 = new ArrayList<Integer>().getClass();
+		System.out.println(c1 == c2);
+	}
+
+}
