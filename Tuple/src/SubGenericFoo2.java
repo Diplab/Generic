6da62d2c95e3a9108d2ee491bb1c964c37c4009e
yingjhu <yingjhu@hotmail.com>
2014-10-14 16:21:08 +0800

    SubGenericFoo2

---
+++ Tuple/src/SubGenericFoo2.java
@@ -0,0 +1,13 @@
+
+public class SubGenericFoo2<T1, T2, T3> 
+                extends GenericFoo2<T1, T2> {
+    private T3 foo3;
+  
+    public void setFoo3(T3 foo3) {
+        this.foo3 = foo3;
+    }
+ 
+    public T3 getFoo3() {
+        return foo3;
+    }
+}
