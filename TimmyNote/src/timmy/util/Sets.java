4c72208a6c27e049fd1b165eed931cd01b3e5b4f
Timmy00274672 <Timmy00274672@gmail.com>
2014-10-15 16:55:21 +0800

    Add overload method(EnumSet) to Sets for effeciency

--- TimmyNote/src/timmy/util/Sets.java
+++ TimmyNote/src/timmy/util/Sets.java
@@ -1,5 +1,6 @@
 package timmy.util;
 
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -28,4 +29,26 @@ public class Sets {
     public static <E> Set<E> complement(Set<E> set1, Set<E> set2) {
 	return difference(union(set1, set2), intersection(set1, set2));
     }
+
+    public static <E extends Enum<E>> Set<E> union(EnumSet<E> set1,
+	    EnumSet<E> set2) {
+	Set<E> resultSet = set1.clone();
+	resultSet.addAll(set2);
+	return resultSet;
+    }
+    
+    public static <E extends Enum<E>> Set<E> intersection(EnumSet<E> set1,
+	    EnumSet<E> set2) {
+	Set<E> resultSet = set1.clone();
+	resultSet.retainAll(set2);
+	return resultSet;
+    }
+    
+    public static <E extends Enum<E>> Set<E> difference(EnumSet<E> set1,
+	    EnumSet<E> set2) {
+	Set<E> resultSet = set1.clone();
+	resultSet.removeAll(set2);
+	return resultSet;
+    }
+
 }
