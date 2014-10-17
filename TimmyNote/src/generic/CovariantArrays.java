57c9d120b912f3e15301308c7e9b309367a7c112
Timmy00274672 <Timmy00274672@gmail.com>
2014-10-17 10:50:23 +0800

    Add generics and covariance sample

--- TimmyNote/src/generic/CovariantArrays.java
+++ TimmyNote/src/generic/CovariantArrays.java
@@ -1,5 +1,8 @@
 package generic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /***
  * Generic is designed to solve below problem. Make this kind of error found in
  * compile time.
@@ -18,15 +21,44 @@ public class CovariantArrays {
 	} catch (ArrayStoreException e) {
 	    System.err.println(e);
 	}
+
+	ArrayList<Apple> arrayList = new ArrayList<Apple>();
+	arrayList.add(new Apple());
+	List<? extends Fruit> fList = arrayList; // upper cast make it cannot do
+						 // anything
+	System.out.println(fList.getClass().getSimpleName());
+
+	// The method add(capture#4-of ? extends Fruit) in the type
+	// List<capture#4-of ? extends Fruit> is not applicable for the
+	// arguments (xxx)
+	// fList.add(new Oragne());
+	// fList.add(new Apple());
+	// fList.add(new Fruit());
+	// fList.add(new Object());
+
+	System.out.println(fList.get(0).getClass().getSimpleName());
+	fList.get(0).f(); // it seems to be Fruit not Apple
+
+	/*
+	 * Unresolved compilation problem: The method a() is undefined for the
+	 * type capture#7-of ? extends Fruit
+	 */
+	// fList.get(0).a();
     }
 
 }
 
 class Fruit {
+    void f() {
+    }
 }
 
 class Apple extends Fruit {
+    void a() {
+    }
 }
 
 class Oragne extends Fruit {
+    void o() {
+    }
 }
