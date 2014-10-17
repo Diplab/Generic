c31bb87d13238fccaf49a7f419ba923629eefc79
Timmy00274672 <Timmy00274672@gmail.com>
2014-10-17 11:12:14 +0800

    Add another example to demo the limit of compiler

---
+++ TimmyNote/src/generic/Holder.java
@@ -0,0 +1,49 @@
+package generic;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+public class Holder<T> {
+    private T value;
+
+    public Holder() {
+    }
+
+    public Holder(T valueT) {
+	this.value = valueT;
+    }
+
+    public T getValue() {
+	return value;
+    }
+
+    public void setValue(T value) {
+	this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (obj instanceof Holder) {
+	    Holder<?> tmpHolder = (Holder<?>) obj;
+	    return value.equals(tmpHolder.getValue());
+	}
+	return false;
+    }
+
+    public static void main(String[] args) {
+	Holder<Apple> appleHolder = new Holder<>(new Apple());
+	appleHolder.getValue().a(); // it's Apple.
+
+	// another sample:
+	Holder<? extends Fruit> fHolder = appleHolder;
+	fHolder.getValue().f(); // it's Fruit
+	((Apple) fHolder.getValue()).a(); // it can be cast to Apple
+
+	/*
+	 * The method setValue(capture#5-of ? extends Fruit) in the type
+	 * Holder<capture#5-of ? extends Fruit> is not applicable for the
+	 * arguments (XXX)
+	 */
+	// fHolder.setValue(new Apple());
+	// fHolder.setValue(new Fruit());
+    }
+}
